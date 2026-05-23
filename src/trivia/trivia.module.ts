65b64870186db31f5d352c5f61a8a00205720f25
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2026-05-23 10:45:39 -0500

    Ultimos ajustes realizados.

--- src/trivia/trivia.module.ts
+++ src/trivia/trivia.module.ts
@@ -1,12 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../../prisma/prisma.module';
-import { TriviaController } from './trivia.controller';
-import { TriviaService } from './trivia.service';
 
-@Module({
-  imports: [PrismaModule],
-  controllers: [TriviaController],
-  providers: [TriviaService],
-  exports: [TriviaService],
-})
-export class TriviaModule { }
+@Module({})
+export class TriviaModule {}
