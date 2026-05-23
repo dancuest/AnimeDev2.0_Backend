b18c7fd11045714b0006f56c66a2c530b8ceda4d
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2026-05-22 21:13:15 -0500

    creacion y mejora de banco de preguntas y moderacion de trivas

--- src/modules/trivia-admin/trivia-admin.module.ts
+++ src/modules/trivia-admin/trivia-admin.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AnimeModule } from '../../anime/anime.module';
+import { TriviaAdminController } from './trivia-admin.controller';
+import { TriviaAdminService } from './trivia-admin.service';
 
-@Module({})
-export class TriviaAdminModule {}
+@Module({
+    imports: [AnimeModule],
+    controllers: [TriviaAdminController],
+    providers: [TriviaAdminService],
+    exports: [TriviaAdminService],
+})
+export class TriviaAdminModule { }
